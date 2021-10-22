d340572fc5dd27c6127aa9a7de8734f2571ee106
Greg Tyler <gregtyler@users.noreply.github.com>
2021-10-22 09:06:47 +0100

    Migrate console to laminas-cli (#3)
    We now support using either `public/index.php` (laminas-mvc-console) or the bins (laminas-cli).
    
    Once we've migrated opg-sirius's Makefile to use the bins too, we can remove laminas-mvc-console and its config/controller.
    
    #minor

---
+++ module/Application/src/Application/Console/ImportFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\Console;
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportFixtures extends Command
+{
+    public function __construct(
+        private ORMPurger $purger,
+        private Loader $loader,
+        private ORMExecutor $executor,
+        private array $fixtureDirectories
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('append', null, InputOption::VALUE_NONE, 'The first ID or starting date to index from')
+            ->addOption('purge-with-truncate', null, InputOption::VALUE_NONE, 'The first ID or starting date to index from');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var bool $append */
+        $append = $input->getOption('append');
+
+        /** @var bool $truncate */
+        $truncate = $input->getOption('purge-with-truncate');
+
+        if ($truncate) {
+            $this->purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
+        }
+
+        foreach ($this->fixtureDirectories as $dir) {
+            $this->loader->loadFromDirectory($dir);
+        }
+
+        $fixtures = $this->loader->getFixtures();
+
+        if ($fixtures) {
+            $output->writeln(sprintf('Loading %d fixtures', count($fixtures)));
+            $this->executor->execute($fixtures, $append);
+        } else {
+            $output->writeln('<fg=yellow>No fixtures found</>');
+        }
+
+        $output->writeln('<fg=green>DONE</>');
+
+        return 0;
+    }
+}
